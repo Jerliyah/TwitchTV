014ee62aab4f065e2e553884d2dd72012ea05830
Jerliyah <jerliyahcraig@gmail.com>
2017-05-30 17:47:41 -0500

    option toggles

--- scripts/action.js
+++ scripts/action.js
@@ -1,8 +1,19 @@
+// Grab the All, Online, and Offline options
+let buttons = document.querySelectorAll('li');
 
-let button = document.querySelector('button');
+// Add event listener to each one
+for(let i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', make_active);
+}
 
+// Toggle active class for options
+function make_active() {
+    // Remove activity from any previous button clicks
+    $(this).siblings().removeClass("active");
+    // Add active clas to current button
+    $(this).addClass("active");
+}
 
-button.addEventListener('click', API_call)
 
 // Using TwitchSample.js due to FreeCodeCamp found errors with the original API
 function API_call() {
